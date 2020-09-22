d28ac4bef0a97abe225af2896bd0f663691d9874
Mikhail Movchan <devcore.ru@gmail.com>
2020-09-22 05:46:01 +0300

    add new flags

--- client/client.go
+++ client/client.go
@@ -4,6 +4,7 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
+	"crypto/sha256"
 	"flag"
 	"fmt"
 	"io"
@@ -66,38 +67,62 @@ func encrypt(key, data []byte) []byte {
 
 func main() {
 
+	encrypting := flag.Bool("e", false, "encrypting file ")
+	decrypting := flag.Bool("d", false, "decrypting file")
+	local := flag.Bool("l", false, "save file local")
 	fileName := flag.String("f", "", "file name")
 	password := flag.String("p", "", "password")
-	server := flag.String("s", "127.0.0.1:9005", "server ip")
+	server := flag.String("s", "", "server ip:port")
+	outFileName := flag.String("o", "config.conf", "output file name")
 
 	flag.Parse()
+	fmt.Println(*fileName)
+	if *fileName == "" {
+		panic("no file name!")
+	}
 
+	if *password == "" {
+		panic("no password!")
+	}
+	tmp := *password
 	data, err := ioutil.ReadFile(*fileName)
 	if err != nil {
 		fmt.Println("File reading error", err)
 		return
 	}
+	fmt.Println("File readed")
+	//fmt.Printf("File size: %v\n", len(data))
 
-	data = encrypt([]byte(*password), data)
+	if *server != "" {
+		conn, err := net.DialTimeout("tcp", *server, 2*time.Second)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer conn.Close()
 
-	fmt.Printf("File size: %v\n", len(data))
+		conn.Write(Cmd.SendFile)
+		conn.Write([]byte(strconv.Itoa(len(data))))
+		conn.Write(data)
 
-	conn, err := net.DialTimeout("tcp", *server, 2*time.Second)
-	if err != nil {
-		fmt.Println(err)
-		return
+		if err != nil {
+			log.Printf("Stream copy error: %s", err)
+		}
+		bufSha256 := make([]byte, 64)
+		conn.Read(bufSha256)
+		fmt.Printf("SHA-256: %s\n", string(bufSha256))
 	}
-	defer conn.Close()
 
-	conn.Write(Cmd.SendFile)
-	conn.Write([]byte(strconv.Itoa(len(data))))
-	conn.Write(data)
+	if *local {
+		if *encrypting {
+			data = encrypt([]byte(tmp), data)
+			ioutil.WriteFile(fmt.Sprintf("%x", sha256.Sum256(data)), data, 0644)
+		}
 
-	if err != nil {
-		log.Printf("Stream copy error: %s", err)
+		if *decrypting {
+			data = decrypt([]byte(tmp), data)
+			ioutil.WriteFile(*outFileName, data, 0644)
+		}
 	}
-	bufSha256 := make([]byte, 64)
-	conn.Read(bufSha256)
-	fmt.Printf("SHA-256: %s\n", string(bufSha256))
 
 }
