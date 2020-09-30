7e600d7451a5cdab6eef68d49e2e3c1666981056
Mikhail Movchan <devcore.ru@gmail.com>
2020-09-30 15:48:53 +0300

    add argv

--- client/main.go
+++ client/main.go
@@ -1,8 +1,40 @@
 package main
 
-func init() {
+import (
+	"flag"
+)
 
+var (
+	//BuildInfo string = ``
+
+	argv struct {
+		//port    uint
+		server  string
+		local   bool
+		decrypt bool
+		encrypt bool
+		file    string
+		outname string
+		passwd  string
+		help    bool
+	}
+)
+
+func init() {
+	flag.StringVar(&argv.server, `s`, `localhost:9005`, `remote conf server`)
+	flag.BoolVar(&argv.help, `h`, false, `show this help`)
+	flag.BoolVar(&argv.local, `l`, false, `enable local save file`)
+	flag.BoolVar(&argv.decrypt, `d`, false, `encrypt file`)
+	flag.BoolVar(&argv.encrypt, `e`, false, `decrypt file`)
+	flag.StringVar(&argv.file, `f`, ``, `open file name`)
+	flag.StringVar(&argv.outname, `o`, `config.conf`, `out file name`)
+	flag.StringVar(&argv.passwd, `p`, ``, `password for crypt`)
+	flag.Parse()
 }
 
 func main() {
+	if argv.help {
+		flag.Usage()
+		return
+	}
 }
