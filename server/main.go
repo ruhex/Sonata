331904b440cd67e9cbf5e7eb07b3825b6466e575
Mikhail Movchan <devcore.ru@gmail.com>
2020-10-07 13:54:31 +0300

    add goroutine for net.Conn

--- server/main.go
+++ server/main.go
@@ -28,36 +28,40 @@ func main() {
 	if err != nil {
 		log.Printf("%s", err)
 	}
-
+	defer ln.Close()
 	for {
 		conn, err := ln.Accept()
-		defer conn.Close()
+		//defer conn.Close()
 		if err != nil {
 			fmt.Printf("Connect error: %s", err)
 		}
-		bufCmd := make([]byte, 1)
-		buffSize := make([]byte, 16)
-		conn.Read(bufCmd)
-		if string(bufCmd) == "a" {
-			println("command: a")
-			conn.Read(buffSize)
-			size, err := strconv.Atoi(fmt.Sprintf("%s", buffSize[0:getSize(buffSize)]))
-			if err != nil {
-				fmt.Printf("error: %v", err)
+
+		go func(c net.Conn) {
+			bufCmd := make([]byte, 1)
+			buffSize := make([]byte, 16)
+			c.Read(bufCmd)
+			if string(bufCmd) == "a" {
+				println("command: a")
+				c.Read(buffSize)
+				size, err := strconv.Atoi(fmt.Sprintf("%s", buffSize[0:getSize(buffSize)]))
+				if err != nil {
+					fmt.Printf("error: %v", err)
+				}
+				fmt.Printf("size: %v\n", size)
+				buffFile := make([]byte, size)
+
+				n, err := c.Read(buffFile)
+				if err != nil {
+					fmt.Printf("error: %v\n", err)
+				}
+				fmt.Printf("len1: %v", n)
+
+				fmt.Printf("buffer: %v\n", len(buffFile))
+				ioutil.WriteFile(fmt.Sprintf("./files/%x", sha256.Sum256(buffFile)), buffFile, 0644)
 			}
-			fmt.Printf("size: %v\n", size)
-			buffFile := make([]byte, size)
-
-			n, err := conn.Read(buffFile)
-			if err != nil {
-				fmt.Printf("error: %v\n", err)
-			}
-			fmt.Printf("len1: %v", n)
-
-			fmt.Printf("buffer: %v\n", len(buffFile))
-			ioutil.WriteFile(fmt.Sprintf("./files/%x", sha256.Sum256(buffFile)), buffFile, 0644)
-		}
-		conn.Write([]byte("ok"))
-
+			c.Write([]byte("ok"))
+			c.Close()
+		}(conn)
 	}
+
 }
